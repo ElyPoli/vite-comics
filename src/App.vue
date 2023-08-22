4f8099e27a1dcca2177d1932e69aecf7e38b5315
ElyPoli <itsos.poli@gmail.com>
2023-08-22 19:00:40 +0200

    added banner

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script>
 import TheHeader from './components/TheHeader.vue';
 import TheFooter from './components/TheFooter.vue';
+import IndexBanner from './components/IndexBanner.vue';
 
 export default {
   components: {
     TheHeader,
     TheFooter,
+    IndexBanner,
   },
   data() {
     return {}
@@ -15,6 +17,12 @@ export default {
 
 <template>
   <TheHeader></TheHeader>
+  <main class="index-main pt-4 pb-4">
+    <div class="container">
+      <p class="text-white fw-bold fs-5 m-0 p-0">&lt;-- Content goes here --&gt;</p>
+    </div>
+  </main>
+  <IndexBanner></IndexBanner>
   <TheFooter></TheFooter>
 </template>
 
---
+++ src/components/IndexBanner.vue
@@ -0,0 +1,45 @@
+<script>
+export default {
+
+};
+</script>
+
+<template>
+    <div class="banner">
+        <div class="container pt-5 pb-5 d-flex align-items-center justify-content-around">
+            <div class="d-flex align-items-center justify-content-around">
+                <img src="/public/img/buy-comics-digital-comics.png" alt="digital comics" class="pe-4">
+                <p class="text-white text-uppercase m-0 p-0">digital comics</p>
+            </div>
+            <div class="d-flex align-items-center justify-content-around">
+                <img src="/public/img/buy-comics-merchandise.png" alt="merchandise" class="pe-4">
+                <p class="text-white text-uppercase m-0 p-0">dc merchandise</p>
+            </div>
+            <div class="d-flex align-items-center justify-content-around">
+                <img src="/public/img/buy-comics-subscriptions.png" alt="subscriptions" class="pe-4">
+                <p class="text-white text-uppercase m-0 p-0">subscriptions</p>
+            </div>
+            <div class="d-flex align-items-center justify-content-around">
+                <img src="/public/img/buy-comics-shop-locator.png" alt="shop locator" class="pe-4">
+                <p class="text-white text-uppercase m-0 p-0">comic shop locator</p>
+            </div>
+            <div class="d-flex align-items-center justify-content-around">
+                <img src="/public/img/buy-dc-power-visa.svg" alt="power visa" class="pe-4">
+                <p class="text-white text-uppercase m-0 p-0">dc power visa</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use "../styles/partials/variables" as *;
+
+.banner {
+    background-color: $primary-color;
+
+    img {
+        height: 50px;
+    }
+}
+
+</style>
