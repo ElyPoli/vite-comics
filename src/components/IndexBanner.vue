ebd463c27909ed33fa17b3c5f6e3d6b4e42ae0d2
ElyPoli <itsos.poli@gmail.com>
2023-08-23 20:24:35 +0200

    added dynamic content to the banner

--- src/components/IndexBanner.vue
+++ src/components/IndexBanner.vue
@@ -1,40 +1,41 @@
 <script>
 export default {
-
+    data() {
+        return {
+            bannerList: [
+                {
+                    name: "digital comics",
+                    srcImg: "/public/img/buy-comics-digital-comics.png",
+                },
+                {
+                    name: "dc merchandise",
+                    srcImg: "/public/img/buy-comics-merchandise.png",
+                },
+                {
+                    name: "subscriptions", 
+                    srcImg: "/public/img/buy-comics-subscriptions.png",
+                },
+                {
+                    name: "comic shop locator", 
+                    srcImg: "/public/img/buy-comics-shop-locator.png",
+                },
+                {
+                    name: "dc power visa", 
+                    srcImg: "/public/img/buy-dc-power-visa.svg",
+                },
+            ],
+        };
+    },
 };
 </script>
 
 <template>
     <div class="banner pt-4 pb-4 overflow-hidden">
         <div class="row justify-content-center">
-            <div class="col-12 col-sm-6 col-md-4 col-lg-2">
+            <div class="col-12 col-sm-6 col-md-4 col-lg-2" v-for="banner in bannerList">
                 <div class="d-flex align-items-center justify-content-center">
-                    <img src="/public/img/buy-comics-digital-comics.png" alt="digital comics" class="pe-2">
-                    <p class="text-white text-uppercase text-nowrap m-0 p-0">digital comics</p>
-                </div>
-            </div>
-            <div class="col-12 col-sm-6 col-md-4 col-lg-2">
-                <div class="d-flex align-items-center justify-content-center pt-sm-0 pt-4">
-                    <img src="/public/img/buy-comics-merchandise.png" alt="merchandise" class="pe-2">
-                    <p class="text-white text-uppercase text-nowrap m-0 p-0">dc merchandise</p>
-                </div>
-            </div>
-            <div class="col-12 col-sm-6 col-md-4 col-lg-2">
-                <div class="d-flex align-items-center justify-content-center pt-md-0 pt-sm-4 pt-4">
-                    <img src="/public/img/buy-comics-subscriptions.png" alt="subscriptions" class="pe-2">
-                    <p class="text-white text-uppercase text-nowrap m-0 p-0">subscriptions</p>
-                </div>
-            </div>
-            <div class="col-12 col-sm-6 col-md-4 col-lg-2">
-                <div class="d-flex align-items-center justify-content-center pt-lg-0 pt-sm-4 pt-4">
-                    <img src="/public/img/buy-comics-shop-locator.png" alt="shop locator" class="pe-2">
-                    <p class="text-white text-uppercase text-nowrap m-0 p-0">comic shop locator</p>
-                </div>
-            </div>
-            <div class="col-12 col-sm-6 col-md-4 col-lg-2">
-                <div class="d-flex align-items-center justify-content-center pt-lg-0 pt-sm-4 pt-4">
-                    <img src="/public/img/buy-dc-power-visa.svg" alt="power visa" class="pe-2">
-                    <p class="text-white text-uppercase text-nowrap m-0 p-0">dc power visa</p>
+                    <img v-bind:src="banner.srcImg" v-bind:alt="banner.name" class="pe-2">
+                    <p class="text-white text-uppercase text-nowrap m-0 p-0"> {{ banner.name }} </p>
                 </div>
             </div>
         </div>
