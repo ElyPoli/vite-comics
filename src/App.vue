4e96d85f4837f218f9c35d575dab82efdb2d5b61
ElyPoli <itsos.poli@gmail.com>
2023-08-23 16:48:54 +0200

    added card main

--- src/App.vue
+++ src/App.vue
@@ -2,28 +2,63 @@
 import TheHeader from './components/TheHeader.vue';
 import TheFooter from './components/TheFooter.vue';
 import IndexBanner from './components/IndexBanner.vue';
+import CurrentSeries from './components/CurrentSeries.vue';
+import comics from './db/comics';
 
 export default {
   components: {
     TheHeader,
     TheFooter,
     IndexBanner,
+    CurrentSeries,
   },
   data() {
-    return {}
+    return {
+      comics,
+    }
   },
 }
 </script>
 
 <template>
   <TheHeader></TheHeader>
-  <main class="index-main pt-4 pb-4">
+  <main class="comics-main pt-5 pb-3">
     <div class="container">
-      <p class="text-white fw-bold fs-5 m-0 p-0">&lt;-- Content goes here --&gt;</p>
+      <div class="row">
+        <div class="col-6 col-sm-4 col-md-3 col-lg-2" v-for="(comicSingle, i) in comics" :key="`comicSingle_${i}`">
+          <CurrentSeries v-bind:thumb="comicSingle.thumb" v-bind:series="comicSingle.series">
+          </CurrentSeries>
+        </div>
+      </div>
+      <div class="d-flex align-items-center justify-content-center pt-2">
+        <button type="button" class="btn my-btn-comics text-uppercase">
+          load more
+        </button>
+      </div>
     </div>
   </main>
   <IndexBanner></IndexBanner>
   <TheFooter></TheFooter>
 </template>
 
-<style></style>
+<style lang="scss" scoped>
+@use "styles/partials/variables" as *;
+
+.comics-main {
+  background-color: #1c1c1c;
+}
+
+.my-btn-comics {
+  color: #ffffff;
+  background-color: $primary-color;
+  border: 2px solid $primary-color;
+  border-radius: 0%;
+  transition: scale .2s;
+  font-weight: bold;
+  font-size: .8rem;
+}
+
+.my-btn-comics:hover {
+  transform: scale(.95);
+}
+</style>
---
+++ src/components/CurrentSeries.vue
@@ -0,0 +1,46 @@
+<script>
+export default {
+    props: {
+        thumb: String,
+        price: String,
+        series: String,
+        type: String,
+    }
+}
+</script>
+
+<template>
+    <div class="comics-list pt-3 d-flex justify-content-center">
+        <div>
+            <img v-bind:src="thumb" v-bind:alt="series">
+            <p class="text-white text-uppercase m-0 p-0 pt-2"> {{ series }} </p>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use "../styles/partials/variables" as *;
+
+.comics-list {
+    cursor: pointer;
+
+    img {
+        height: 135px;
+        width: 120px;
+        object-fit: cover;
+        object-position: top;
+        transition: filter 0.3s;
+    }
+
+    p {
+        font-size: .8rem;
+        width: 120px;
+    }
+}
+
+.comics-list:hover {
+    img {
+        filter: brightness(0.6);
+    }
+}
+</style>
